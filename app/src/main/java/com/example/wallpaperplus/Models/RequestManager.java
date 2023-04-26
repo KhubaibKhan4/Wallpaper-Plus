411c553b38658fbc19fb0962c04bcba065dd487f
Khubaib Khan <18.bscs.803@gmail.com>
2023-04-27 01:41:23 +0500

    Wallpaper Plus

--- app/src/main/java/com/example/wallpaperplus/Models/RequestManager.java
+++ app/src/main/java/com/example/wallpaperplus/Models/RequestManager.java
@@ -82,7 +82,7 @@ public class RequestManager implements Serializable {
     public interface CuratedApi {
         @Headers({
                 "Accept: application/json",
-                "Authorization: psWUxJSiSZbvIQh0qDy6CcKDS4dsBl7fs3L0dzjcxSxe4kYSxAltOS0I"
+                "Authorization: Your Api Key"
         })
         @GET("curated")
         Call<CuratedApiResponse> getWallpaper(
@@ -95,7 +95,7 @@ public class RequestManager implements Serializable {
     public interface SearchApi {
         @Headers({
                 "Accept: application/json",
-                "Authorization: psWUxJSiSZbvIQh0qDy6CcKDS4dsBl7fs3L0dzjcxSxe4kYSxAltOS0I"
+                "Authorization: Your Api Key"
         })
         @GET("search")
         Call<SearchApiResponse> getWallpaper(
